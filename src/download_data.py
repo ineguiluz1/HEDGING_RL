e45caca6a17a710eff2db85f38718bc3dc7847a1
aitorDiezMateo <aitor.diez@opendeusto.es>
2025-11-18 17:22:50 +0100

    Upload full data

--- src/download_data.py
+++ src/download_data.py
@@ -3,7 +3,7 @@ import yfinance as yf
 ticker = "^GSPC"  # También podrías usar "SPY" (ETF)
 
 # Descarga datos históricos diarios de los últimos 20 años
-data = yf.download(ticker, start="2003-11-17", end="2023-11-17", progress=True)
+data = yf.download(ticker, period="max", progress=True)
 
 # Guarda los datos en un archivo CSV
 data.to_csv('data/sp500_data.csv')
